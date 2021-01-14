09ec9c904bfda21c6775b30c60387c095e4b7a7b
Markus Seidler <mseidler79@gmail.com>
2021-01-14 11:50:19 +0800

    Initial Commit

---
+++ SetGame2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame2021
+//
+//  Created by Markus Seidler on 14/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SetGame2021/SetGame2021App.swift
@@ -0,0 +1,17 @@
+//
+//  SetGame2021App.swift
+//  SetGame2021
+//
+//  Created by Markus Seidler on 14/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGame2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
