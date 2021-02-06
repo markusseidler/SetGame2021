fb1c60369b411a4c02093a8194c98ee7c5206375
Markus Seidler <mseidler79@gmail.com>
2021-02-06 22:16:10 +0800

    Uncommented simple UI tests

--- SetGame2021UITests/GameViewTests.swift
+++ SetGame2021UITests/GameViewTests.swift
@@ -14,7 +14,7 @@ import XCTest
 
 // TODO: - How to test views appearing on screen? views which are not buttons or texts?
 // TODO: - work on game flow logic... three selected -> check match -> unselect... or fly away
-`
+
 
 class GameViewTests: XCTestCase {
     
@@ -37,42 +37,42 @@ class GameViewTests: XCTestCase {
       
     }
 
-//    func testExample() throws {
-//        // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Use recording to get started writing UI tests.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
-    
-    func testNewGameWithFirstDeal() {
-        let newGameButton = app.buttons["newGameButton"]
-        let firstDealButton = app.buttons["firstDealButton"]
-        
-        XCTAssertTrue(newGameButton.exists)
-        XCTAssertTrue(firstDealButton.exists)
-        
-        newGameButton.tap()
-        firstDealButton.tap()
-        
-        
-        
-        sleep(8)
-        
-//        XCTAssertTrue(app.staticTexts["singleCardView"].exists)
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
 
-        
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+    
+//    func testNewGameWithFirstDeal() {
+//        let newGameButton = app.buttons["newGameButton"]
+//        let firstDealButton = app.buttons["firstDealButton"]
+//
+//        XCTAssertTrue(newGameButton.exists)
+//        XCTAssertTrue(firstDealButton.exists)
+//
+//        newGameButton.tap()
+//        firstDealButton.tap()
+//
+//
+//
+//        sleep(8)
+//
+////        XCTAssertTrue(app.staticTexts["singleCardView"].exists)
+//
+//
+//    }
     
     
 }
